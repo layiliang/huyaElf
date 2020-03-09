de2af9c09d3c80ce1e611ba9845ab85f03af4240
layiliang <271980982@qq.com>
2020-03-09 23:14:09 +0800

    aaa

--- huyaElf/src/main/resources/templates/竞猜.js
+++ huyaElf/src/main/resources/templates/竞猜.js
@@ -1,45 +1,29 @@
 
-setInterval(function(){
-	$($(".guess-btn")[0]).click();
-	setTimeout(function(){
-		var leftBean1 = $($(".left-bean")[0]).text().substring(5);
-		$($(".guess-btn")[1]).click();
-		setTimeout(function(){
-			var leftBean2 = $($(".left-bean")[0]).text().substring(5);
-			console.log(leftBean1 + "                     aaa                " + leftBean2);
-		},200);
-	},200);
-},2000);
-
-
+var rate1 = 
+var rate2 = 
+var leftBean1;
+var leftBean2;
 var leftBean = $($(".left-bean")[0]).text().substring(5);
 $($(".guess-btn")[0]).click();
-while(true){
-	var leftBean1 = $($(".left-bean")[0]).text().substring(5);
-	if(leftBean1!=leftBean){
-		leftBean = leftBean1;
-		while(true){
-			var leftBean2 = $($(".left-bean")[1]).text().substring(5);
-			if(leftBean2!=leftBean){
-				console.log(leftBean1 + "     aaa     " + leftBean2)
-				break;
-			}
-		}
-		break;
-	}
-}
-
-
-
-
-var leftBean = $($(".left-bean")[0]).text().substring(5);
-$($(".guess-btn")[1]).click();
 var interval1 = setInterval(function(){
-	var leftBean1 = $($(".left-bean")[0]).text().substring(5);
-	console.log("aa   "+leftBean1);
+	leftBean1 = $($(".left-bean")[0]).text().substring(5);
+	console.log("leftBean1的初始值：  "+leftBean1);
 	if(leftBean1!=leftBean){
-		console.log(leftBean1);
+		console.log("leftBean1的真实值：  "+leftBean1);
 		clearInterval(interval1);
+		$($(".guess-btn")[1]).click();
+		var interval2 = setInterval(function(){
+			leftBean2 = $($(".left-bean")[0]).text().substring(5);
+			if(leftBean1!=leftBean2){
+				clearInterval(interval2);
+				if(leftBean1>leftBean2){
+					
+				}else{
+					
+				}
+				console.log("leftBean1，2的真实值分别是：  "+leftBean1 + "," + leftBean2)
+			}
+		},1);
 	}
 },1);
 
