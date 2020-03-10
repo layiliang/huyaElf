4a6fcfc9793b6dbb2a42461c01d7cbe87fda1ef4
layiliang <271980982@qq.com>
2020-03-10 17:43:45 +0800

    aaa

--- huyaElf/src/main/java/com/lyl/webElf/test/Test2.java
+++ huyaElf/src/main/java/com/lyl/webElf/test/Test2.java
@@ -2,47 +2,20 @@ package com.lyl.webElf.test;
 
 public class Test2 {
 	public static void main(String[] args) {
-		Zi1 z1 = new Zi1();
-		Zi2 z2 = new Zi2();
-		Ele ele = new Ele();
-		ele.setA(1);
-		Foo foo=  new Foo();
-		foo.setEle(ele);
-		ele.setA(15);
-		System.out.println(z1.getEle().a);
-		System.out.println(z2.getEle().a);
+		int num1 = 10000;
+		double rate1 = 1.2;
+		double rate2 = 0.95;
+		int num2 = (int) (num1*(0.95*rate1 + 1)/(0.95*rate2 + 1));
+		System.out.println(num1+num2);
+		System.out.println(num1*(0.95*rate1 - (0.95*rate1 + 1)/(0.95*rate2 + 1)));
+		System.out.println(num2*(0.95*rate2 - (0.95*rate2 + 1)/(0.95*rate1 + 1)));
+		System.out.println((0.95*rate1 - (0.95*rate1 + 1)/(0.95*rate2 + 1))/(1 + (0.95*rate1 + 1)/(0.95*rate2 + 1)));
+		System.out.println(151.64257555847493/(num1+num2));
+		System.out.println("-------");
+		System.out.println((0.95*0.95*rate1*rate2 - 1)/(0.95*rate1+0.95*rate2+2));
 		
+		System.out.println((0.95*rate2 - (0.95*rate2 + 1)/(0.95*rate1 + 1))/((0.95*rate2 + 1)/(0.95*rate1 + 1) + 1));
+		
+		System.out.println(21248/(1+(0.95*rate1 + 1)/(0.95*rate2 + 1)));
 	}
-}
-
-class Foo{
-	Ele ele =null;
-
-	public Ele getEle() {
-		return ele;
-	}
-
-	public void setEle(Ele ele) {
-		this.ele = ele;
-	}
-	
-	
-}
-class Zi1 extends Foo{
-	
-}
-class Zi2 extends Foo{
-	
-}
-class Ele{
-	int a;
-
-	public int getA() {
-		return a;
-	}
-
-	public void setA(int a) {
-		this.a = a;
-	}
-	
 }
--- huyaElf/src/main/resources/templates/竞猜.js
+++ huyaElf/src/main/resources/templates/竞猜.js
@@ -1,30 +1,70 @@
 
-var rate1 = 
-var rate2 = 
-var leftBean1;
-var leftBean2;
-var leftBean = $($(".left-bean")[0]).text().substring(5);
-$($(".guess-btn")[0]).click();
-var interval1 = setInterval(function(){
-	leftBean1 = $($(".left-bean")[0]).text().substring(5);
-	console.log("leftBean1的初始值：  "+leftBean1);
-	if(leftBean1!=leftBean){
-		console.log("leftBean1的真实值：  "+leftBean1);
-		clearInterval(interval1);
-		$($(".guess-btn")[1]).click();
-		var interval2 = setInterval(function(){
-			leftBean2 = $($(".left-bean")[0]).text().substring(5);
-			if(leftBean1!=leftBean2){
-				clearInterval(interval2);
-				if(leftBean1>leftBean2){
-					
-				}else{
-					
+//var rate1 = 
+//var rate2 = 
+	var leftBean1;
+	var leftBean2;
+	var leftBean = $($(".left-bean")[0]).text().substring(5);
+	$($(".guess-btn")[0]).click();
+	var interval1 = setInterval(function(){
+		leftBean1 = $($(".left-bean")[0]).text().substring(5);
+		console.log("leftBean1的初始值：  "+leftBean1);
+		if(leftBean1!=leftBean){
+			console.log("leftBean1的真实值：  "+leftBean1);
+			clearInterval(interval1);
+			$($(".guess-btn")[1]).click();
+			var interval2 = setInterval(function(){
+				leftBean2 = $($(".left-bean")[0]).text().substring(5);
+				if(leftBean1!=leftBean2){
+					clearInterval(interval2);
+					if(leftBean1>leftBean2){
+						
+					}else{
+						
+					}
+					console.log("leftBean1，2的真实值分别是：  "+leftBean1 + "," + leftBean2)
 				}
-				console.log("leftBean1，2的真实值分别是：  "+leftBean1 + "," + leftBean2)
+			},2);
+		}
+	},2);
+	
+	
+
+	
+	
+function sleep(ms) {
+  	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async function() {
+	for(var i = 0 ; i < 10 ; i ++){
+		debugger
+		console.log(i);
+		var leftBean1;
+		var leftBean2;
+		var leftBean = $($(".left-bean")[0]).text().substring(5);
+		await sleep(20);
+		$($(".guess-btn")[0]).click();
+		while(true){
+			leftBean1 = $($(".left-bean")[0]).text().substring(5);
+			console.log("leftBean1的初始值：  "+leftBean1);
+			if(leftBean1 != leftBean){
+				console.log("leftBean1的真实值：  "+leftBean1);
+				break;
 			}
-		},1);
+			await sleep(2);
+		}
+		await sleep(20);
+		$($(".guess-btn")[1]).click();
+		while(true){
+			leftBean2 = $($(".left-bean")[0]).text().substring(5);
+			console.log("leftBean2的初始值：  "+leftBean2);
+			if(leftBean2 != leftBean1){
+				console.log("leftBean2的真实值：  "+leftBean2);
+				break;
+			}
+			await sleep(2);
+		}
+		console.log("leftBean1，2的真实值分别是：  "+leftBean1 + "," + leftBean2)
+		await sleep(2);
 	}
-},1);
-
-
+})()
